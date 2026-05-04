7a226a5fa7bbc7827de6ad4c24062663679cde2d
ashtonguevarra <ashtonguevarra1@gmail.com>
2026-05-04 16:07:26 +0800

    delete temporary script

--- genssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# this script automatically connects github repository to ssh
-
-echo "Available SSH keys:"
-ls -al ~/.ssh
-ssh -T git@github.com
-git remote -v
-echo "Enter your github repository SSH url with 'git remote set-url otigin'"
